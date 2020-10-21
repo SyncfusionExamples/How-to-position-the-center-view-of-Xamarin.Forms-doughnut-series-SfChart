fbc50b2f0201e468851a8cc0fb4acfff9ba7cba0
Hemalatha <hemalathavinai@gmail.com>
2020-10-21 16:16:03 +0530

    Sample included

---
+++ Doughtnut_CenterView/Doughtnut_CenterView/Doughtnut_CenterView/Model/DataModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Doughtnut_CenterView
+{
+    public class DataModel
+    {
+        public string XData { get; set; }
+        public double YData { get; set; }
+    }
+
+}
