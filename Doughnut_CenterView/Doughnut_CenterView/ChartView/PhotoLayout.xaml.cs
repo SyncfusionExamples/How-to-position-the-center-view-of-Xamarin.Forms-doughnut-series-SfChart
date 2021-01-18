5421298f3968e25e49316a8e736114aa2d6f014b
Mohanlal Gopu <mohanlal.gopu@syncfusion.com>
2021-01-18 18:32:08 +0530

    Update new version.

---
+++ Doughnut_CenterView/Doughnut_CenterView/ChartView/PhotoLayout.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Doughnut_CenterView
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PhotoLayout : ContentView
+    {
+        public PhotoLayout()
+        {
+            InitializeComponent();
+        }
+    }
+}
